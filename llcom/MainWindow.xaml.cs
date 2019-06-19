f28f746a15f647f088159cca1dc4eba14f471122
chenxuuu <lolicon@papapoi.com>
2019-06-19 23:31:17 +0800

    快捷发送部分界面设计

--- llcom/MainWindow.xaml.cs
+++ llcom/MainWindow.xaml.cs
@@ -24,5 +24,28 @@ namespace llcom
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<ToSendData> items = new List<ToSendData>();
+            items.Add(new ToSendData() { id = 1, text = "AT", hex = false });
+            items.Add(new ToSendData() { id = 2, text = "ATI", hex = false });
+            items.Add(new ToSendData() { id = 3, text = "AT+CREG?", hex = false });
+            items.Add(new ToSendData() { id = 4, text = "AT+CGATT?", hex = false });
+            items.Add(new ToSendData() { id = 5, text = "AT+CIPSEND=2,0", hex = false });
+            items.Add(new ToSendData() { id = 6, text = "AA BB CC DD", hex = true });
+            items.Add(new ToSendData() { id = 7, text = "11 22 66 22 44", hex = true });
+            for (int i=8;i<=50;i++)
+                items.Add(new ToSendData() { id=i,text="text"+i,hex=false});
+
+            toSendList.ItemsSource = items;
+        }
+    }
+
+    public class ToSendData
+    {
+        public int id { get; set; }
+        public string text { get; set; }
+        public bool hex { get; set; }
     }
 }
