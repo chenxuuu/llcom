6f99d98937b33d16716f307824cc179943ac656c
chenxuuu <lolicon@papapoi.com>
2019-06-22 21:21:41 +0800

    设置页lua测试功能完成

---
+++ llcom/LuaEnv/LuaLoader.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace llcom.LuaEnv
+{
+    class LuaLoader
+    {
+
+        /// <summary>
+        /// 初始化lua对象
+        /// </summary>
+        /// <param name="lua"></param>
+        public static void Initial(NLua.Lua lua)
+        {
+
+        }
+
+        /// <summary>
+        /// 运行lua文件并获取结果
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static string Run(string file, ArrayList args = null)
+        {
+            //文件不存在
+            if (!File.Exists(file))
+                return "";
+
+            using (var lua = new NLua.Lua())
+            {
+                try
+                {
+                    lua.State.Encoding = Encoding.UTF8;
+                    lua.LoadCLRPackage();
+                    lua["runType"] = "send";//一次性处理标志
+                    Initial(lua);
+                    if (args != null)
+                        for (int i = 0; i < args.Count; i += 2)
+                        {
+                            lua[(string)args[i]] = args[i + 1];
+                        }
+                    return (string)lua.DoFile(file)[0];
+                }
+                catch (Exception e)
+                {
+                    throw new Exception(e.ToString());
+                }
+            }
+        }
+    }
+}
--- llcom/Tools/Global.cs
+++ llcom/Tools/Global.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace llcom.Tools
@@ -26,6 +28,36 @@ namespace llcom.Tools
             uart.serial.Parity = (Parity)setting.parity;
             uart.serial.DataBits = setting.dataBits;
             uart.serial.StopBits = (StopBits)setting.stopBit;
+        }
+
+        /// <summary>
+        /// 字符串转hex值
+        /// </summary>
+        /// <param name="str">字符串</param>
+        /// <param name="space">间隔符号</param>
+        /// <returns>结果</returns>
+        public static string String2Hex(string str, string space)
+        {
+             return BitConverter.ToString(Encoding.Default.GetBytes(str)).Replace("-", space);
+        }
+
+
+        /// <summary>
+        /// hex值转字符串
+        /// </summary>
+        /// <param name="mHex">hex值</param>
+        /// <returns>原始字符串</returns>
+        public static string Hex2String(string mHex)
+        {
+            mHex = Regex.Replace(mHex, "[^0-9A-Fa-f]", "");
+            if (mHex.Length % 2 != 0)
+                mHex = mHex.Remove(mHex.Length - 1, 1);
+            if (mHex.Length <= 0) return "";
+            byte[] vBytes = new byte[mHex.Length / 2];
+            for (int i = 0; i < mHex.Length; i += 2)
+                if (!byte.TryParse(mHex.Substring(i, 2), NumberStyles.HexNumber, null, out vBytes[i / 2]))
+                    vBytes[i / 2] = 0;
+            return Encoding.Default.GetString(vBytes);
         }
     }
 }
