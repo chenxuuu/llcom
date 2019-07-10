dbfdd2de9d0f4cbdcfa049681da3fbc3013d4311
chenxuuu <lolicon@papapoi.com>
2019-07-10 11:14:58 +0800

    更新俩at脚本，分为快发和慢发

--- llcom/DefaultFiles/user_script_run/AT控制TCP连接-快发模式.lua
+++ llcom/DefaultFiles/user_script_run/AT控制TCP连接-快发模式.lua
@@ -22,37 +22,58 @@ function rollRun(cmd,receive,timeout)
 end
 
 sys.taskInit(function ()
+    --检查是否可收发at
     log.info("check start")
     rollRun("AT","OK")
 
+    --关闭回显
+    log.info("close back")
+    rollRun("ATE0","OK")
+
+    --查看at版本
     log.info("check version")
     rollRun("ATI","OK")
 
+    --检查卡是否识别
     log.info("check sim card")
     rollRun("AT+CPIN?","+CPIN: READY")
 
+    --检查是否附着上网络
     log.info("check network")
     rollRun("AT+CGATT?","+CGATT: 1")
 
+    --快发模式
+    log.info("set cipmode")
+    rollRun("AT+CIPQSEND=1","OK",5000)
 
+    --非透传
+    log.info("set cipmode")
+    rollRun("AT+CIPMODE=0","OK",5000)
+
+    --设置vpn
     log.info("set apn")
     rollRun('AT+CSTT="CMIOT"',"OK",5000)
 
+    --激活移动场景
     log.info("AT+CIICR")
     rollRun("AT+CIICR","OK",5000)
 
+    --检查ip
     log.info("check ip")
     rollRun("AT+CIFSR","%.",1000)
 
+    --连接服务器
     log.info("connect server")
     rollRun([[AT+CIPSTART="TCP","]]..server..[[",]]..ip,"CONNECT OK",5000)
 
     while true do
+        --发送数据请求
         log.info("start send data")
         rollRun("AT+CIPSEND=10",">",1000)
 
+        --数据内容
         log.info("data content")
-        rollRun("1234567890","1234567890",1000)
+        rollRun("1234567890","DATA ACCEPT",1000)
 
         sys.wait(5000)
     end
