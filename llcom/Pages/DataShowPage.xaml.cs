e334dea7f0815548a4e90d9056a082a5444a9376
chenxuuu <lolicon@papapoi.com>
2019-11-03 17:51:56 +0800

    拆分数据收发日志显示窗口

---
+++ llcom/Pages/DataShowPage.xaml.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace llcom.Pages
+{
+    /// <summary>
+    /// DataShowPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataShowPage : Page
+    {
+        public DataShowPage()
+        {
+            InitializeComponent();
+        }
+
+        ScrollViewer sv;
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            //使日志富文本区域滚动可控制
+            sv = uartDataFlowDocument.Template.FindName("PART_ContentHost", uartDataFlowDocument) as ScrollViewer;
+            Tools.Logger.DataShowEvent += addUartLog;
+            Tools.Logger.DataClearEvent += (xx,x) =>
+            {
+                uartDataFlowDocument.Document.Blocks.Clear();
+            };
+        }
+
+
+        /// <summary>
+        /// 添加串口日志数据
+        /// </summary>
+        /// <param name="data">数据</param>
+        /// <param name="send">true为发送，false为接收</param>
+        private void addUartLog(object e,Tools.DataShowPara input)
+        {
+            byte[] data = input.data;
+            bool send = input.send;
+            Paragraph p = new Paragraph(new Run(""));
+
+            Span text = new Span(new Run(DateTime.Now.ToString("[yyyy/MM/dd HH:mm:ss.ffff]")));
+            text.Foreground = Brushes.DarkSlateGray;
+            p.Inlines.Add(text);
+
+            if (send)
+                text = new Span(new Run(" ← "));
+            else
+                text = new Span(new Run(" → "));
+            text.Foreground = Brushes.Black;
+            text.FontWeight = FontWeights.Bold;
+            p.Inlines.Add(text);
+
+            if (data.Length > 2000)
+                text = new Span(new Run(Tools.Global.Byte2String(data.Skip(0).Take(2000).ToArray())
+                    + "\r\n数据过长，剩余部分请去日志文件查看"));
+            else
+                text = new Span(new Run(Tools.Global.Byte2String(data)));
+
+            if (send)
+                text.Foreground = Brushes.DarkRed;
+            else
+                text.Foreground = Brushes.DarkGreen;
+            text.FontSize = 15;
+            p.Inlines.Add(text);
+
+            if (!Tools.Global.setting.showHex)
+                p.Margin = new Thickness(0, 0, 0, 8);
+            uartDataFlowDocument.Document.Blocks.Add(p);
+
+            if (Tools.Global.setting.showHex)
+            {
+                if (data.Length > 600)
+                    p = new Paragraph(new Run("HEX:" + Tools.Global.Byte2Hex(data.Skip(0).Take(600).ToArray(), " ")
+                    + "\r\n数据过长，剩余部分请去日志文件查看"));
+                else
+                    p = new Paragraph(new Run("HEX:" + Tools.Global.Byte2Hex(data, " ")));
+
+                if (send)
+                    p.Foreground = Brushes.LightPink;
+                else
+                    p.Foreground = Brushes.LightGreen;
+                p.Margin = new Thickness(0, 0, 0, 8);
+                uartDataFlowDocument.Document.Blocks.Add(p);
+            }
+
+            //条目过多，自动清空
+            if (uartDataFlowDocument.Document.Blocks.Count > 500)
+            {
+                uartDataFlowDocument.Document.Blocks.Clear();
+                addUartLog(null,new Tools.DataShowPara {
+                    data = Tools.Global.Hex2Byte(Tools.Global.String2Hex("数据量过大，自动清理，请去日志文件查看历史数据", "")),
+                    send = true
+                });
+            }
+
+            sv.ScrollToBottom();
+        }
+    }
+}
