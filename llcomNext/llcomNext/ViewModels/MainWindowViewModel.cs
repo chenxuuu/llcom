ca01fe06f600e2145705f5275f1ec4a427e66a39
chenxuuu <lolicon@papapoi.com>
2023-02-14 16:03:54 +0800

    add: initial project

---
+++ llcomNext/llcomNext/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace llcomNext.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Hello World!";
+    }
+}
---
+++ llcomNext/llcomTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace llcomTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
