58549e6339987d35ae23a59e9f7a0d0bfc8b0e8b
chenxuuu <lolicon@papapoi.com>
2024-04-26 17:40:31 +0800

    update: 换成nlua

---
+++ llcomNext/llcomTest/Lua.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace llcomTest
+{
+    [TestClass]
+    public class Lua
+    {
+        [TestMethod]
+        public void Basic()
+        {
+            using var env = new NLua.Lua();
+            env.DoString("print('hello world!')");
+        }
+    }
+}
