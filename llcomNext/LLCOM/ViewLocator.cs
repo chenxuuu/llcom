d4a7c184bc6d4997bdd58759f6d66deec19ae56e
chenxuuu <lolicon@papapoi.com>
2025-03-12 19:25:35 +0800

    update:latest version

--- llcomNext/LLCOM/ViewLocator.cs
+++ llcomNext/LLCOM/ViewLocator.cs
@@ -7,9 +7,9 @@ namespace LLCOM
 {
     public class ViewLocator : IDataTemplate
     {
-        public Control Build(object data)
+        public Control Build(object? data)
         {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
+            var name = data!.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
             if (type != null)
@@ -20,7 +20,7 @@ namespace LLCOM
             return new TextBlock { Text = "Not Found: " + name };
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
