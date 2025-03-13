5c69ffa8ad76bc432ddfa7ac91295386a37ea044
chenxuuu <lolicon@papapoi.com>
2025-03-13 22:49:10 +0800

    add: 左侧页面切换按钮布局

--- llcomNext/LLCOM/ViewModels/MainViewModel.cs
+++ llcomNext/LLCOM/ViewModels/MainViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace LLCOM.ViewModels;
 
-public partial class MainViewModel(Func<Type, ViewModelBase?> getService) : ViewModelBase
+public partial class MainViewModel : ViewModelBase
 {
+    private Func<Type, ViewModelBase?> GetService;
+    
+
+    
+    public MainViewModel(Func<Type, ViewModelBase?> getService)
+    {
+        GetService = getService;
+    }
 
 }
