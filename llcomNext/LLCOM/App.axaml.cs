182a3398a223c9827b65794691cc0e64e401806e
chenxuuu <lolicon@papapoi.com>
2025-03-15 23:08:20 +0800

    add: 分包数据样式做成控件

--- llcomNext/LLCOM/App.axaml.cs
+++ llcomNext/LLCOM/App.axaml.cs
@@ -4,54 +4,57 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
+using Avalonia.Metadata;
 using LLCOM.ViewModels;
 using LLCOM.Views;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace LLCOM
+//可以让XAML文件中直接使用自定义的控件
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", "LLCOM.Controls")]
+
+namespace LLCOM;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public override void Initialize()
     {
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+        AvaloniaXamlLoader.Load(this);
+    }
 
-        public override void OnFrameworkInitializationCompleted()
-        {
-            var collection = new ServiceCollection();
-            ServiceProvider? serviceProvider = null;
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var collection = new ServiceCollection();
+        ServiceProvider? serviceProvider = null;
             
-            collection.AddSingleton<MainWindowViewModel>();
-            collection.AddSingleton<MainViewModel>();
-            collection.AddSingleton<DataPageViewModel>();
-            collection.AddSingleton<ToolsPageViewModel>();
-            collection.AddSingleton<ScriptPageViewModel>();
-            collection.AddSingleton<OnlinePageViewModel>();
-            collection.AddSingleton<LogPageViewModel>();
-            collection.AddSingleton<SettingPageViewModel>();
+        collection.AddSingleton<MainWindowViewModel>();
+        collection.AddSingleton<MainViewModel>();
+        collection.AddSingleton<DataPageViewModel>();
+        collection.AddSingleton<ToolsPageViewModel>();
+        collection.AddSingleton<ScriptPageViewModel>();
+        collection.AddSingleton<OnlinePageViewModel>();
+        collection.AddSingleton<LogPageViewModel>();
+        collection.AddSingleton<SettingPageViewModel>();
             
-            collection.AddSingleton<PacketDataViewModel>();
-            collection.AddSingleton<TerminalViewModel>();
-            collection.AddSingleton<WaveformViewModel>();
+        collection.AddSingleton<PacketDataViewModel>();
+        collection.AddSingleton<TerminalViewModel>();
+        collection.AddSingleton<WaveformViewModel>();
 
-            //用于获取别的ViewModel
-            collection.AddSingleton<Func<Type, ViewModelBase>>(x => type => (ViewModelBase)serviceProvider!.GetService(type));
+        //用于获取别的ViewModel
+        collection.AddSingleton<Func<Type, ViewModelBase>>(x => type => (ViewModelBase)serviceProvider!.GetService(type));
             
-            serviceProvider = collection.BuildServiceProvider();
+        serviceProvider = collection.BuildServiceProvider();
             
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Line below is needed to remove Avalonia data validation.
+            // Without this line you will get duplicate validations from both Avalonia and CT
+            BindingPlugins.DataValidators.RemoveAt(0);
+            desktop.MainWindow = new MainWindow
             {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = serviceProvider.GetService<MainWindowViewModel>()
-                };
-            }
-
-            base.OnFrameworkInitializationCompleted();
+                DataContext = serviceProvider.GetService<MainWindowViewModel>()
+            };
         }
+
+        base.OnFrameworkInitializationCompleted();
     }
 }
---
+++ llcomNext/LLCOM/Controls/PacketDataControl.axaml.cs
@@ -0,0 +1,51 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace LLCOM.Controls;
+
+public class PacketDataControl : TemplatedControl
+{
+    //主题色
+    public static readonly StyledProperty<IBrush?> MainColorProperty = 
+        AvaloniaProperty.Register<PacketDataControl, IBrush?>(nameof(MainColor));
+    public IBrush? MainColor
+    {
+        get => GetValue(MainColorProperty);
+        set => SetValue(MainColorProperty, value);
+    }
+    
+    public static readonly StyledProperty<string?> HeaderProperty = 
+        AvaloniaProperty.Register<PacketDataControl, string?>(nameof(Header));
+    public string? Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+    
+    public static readonly StyledProperty<string?> ExtraProperty = 
+        AvaloniaProperty.Register<PacketDataControl, string?>(nameof(Extra));
+    public string? Extra
+    {
+        get => GetValue(ExtraProperty);
+        set => SetValue(ExtraProperty, value);
+    }
+    
+    
+    public static readonly StyledProperty<string?> TextProperty = 
+        AvaloniaProperty.Register<PacketDataControl, string?>(nameof(Text));
+    public string? Text
+    {
+        get => GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+    
+    public static readonly StyledProperty<string?> HexProperty = 
+        AvaloniaProperty.Register<PacketDataControl, string?>(nameof(Hex));
+    public string? Hex
+    {
+        get => GetValue(HexProperty);
+        set => SetValue(HexProperty, value);
+    }
+}
