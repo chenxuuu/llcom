daa26dc50bb7701947d4fd919251342d959c7b38
chenxuuu <lolicon@papapoi.com>
2025-03-21 22:04:18 +0800

    add: 升级提示小弹窗

--- llcomNext/LLCOM/Views/MainView.axaml.cs
+++ llcomNext/LLCOM/Views/MainView.axaml.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Notifications;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using LLCOM.ViewModels;
+using Notification = Ursa.Controls.Notification;
+using WindowNotificationManager = Ursa.Controls.WindowNotificationManager;
 
 namespace LLCOM.Views;
 
 public partial class MainView : UserControl
 {
+    private WindowNotificationManager? _manager;
+    
     public MainView()
     {
         InitializeComponent();
+    }
+    
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        var topLevel = TopLevel.GetTopLevel(this);
+        _manager = new WindowNotificationManager(topLevel) { MaxItems = 3 };
     }
 
     private async void Control_OnLoaded(object? sender, RoutedEventArgs e)
     {
         await ((MainViewModel)this.DataContext!).PageLoadedCommand.ExecuteAsync(null);
+        
+        if(Services.Utils.HasUpdate())
+            _manager?.Show(
+                new Notification("提示", "有新版本可用，请前往关于页面查看"),
+                type: NotificationType.Information,
+                classes: ["Light"],
+                expiration: TimeSpan.FromSeconds(60));
     }
 }
