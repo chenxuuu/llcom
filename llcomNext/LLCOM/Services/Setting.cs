874f6303d82758f47f458d9927c256bb3610cc3f
chenxuuu <lolicon@papapoi.com>
2025-03-23 22:37:48 +0800

    update: 优化配置存储代码

--- llcomNext/LLCOM/Services/Setting.cs
+++ llcomNext/LLCOM/Services/Setting.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 
@@ -6,26 +9,42 @@ namespace LLCOM.Services;
 
 public partial class Setting : ObservableObject
 {
-    private static Database Database = new("setting.db");
+    private static readonly Database Database = new("setting.db");
     
     //是否折叠分包数据的标签
     [ObservableProperty]
     private bool _isPacketShowHeader = Database.Get(nameof(IsPacketShowHeader), true).Result;
-    partial void OnIsPacketShowHeaderChanged(bool value) => Database.Set(nameof(IsPacketShowHeader), value).WaitAsync(TimeSpan.FromSeconds(2));
 
     //是否转义不可见字符
     [ObservableProperty]
     private bool _isEscapeInvisibleChar = Database.Get(nameof(IsEscapeInvisibleChar), true).Result;
-    partial void OnIsEscapeInvisibleCharChanged(bool value) => Database.Set(nameof(IsEscapeInvisibleChar), value).WaitAsync(TimeSpan.FromSeconds(2));
     
     //Hex显示模式
     [ObservableProperty]
     [NotifyPropertyChangedFor(nameof(IsShowString),nameof(IsShowHexSmall))]
     private bool? _isHexMode = Database.Get(nameof(IsHexMode), (bool?)null).Result;
-    partial void OnIsHexModeChanged(bool? value) => Database.Set(nameof(IsHexMode), value).WaitAsync(TimeSpan.FromSeconds(2));
     
     //是否显示字符串，给Hex显示模式使用，不存储
     public bool IsShowString => IsHexMode is null || !IsHexMode.Value;
     //是否显示hex小字，给Hex显示模式使用，不存储
     public bool IsShowHexSmall => IsHexMode is null;
+
+    public Setting()
+    {
+        PropertyChanged += async (sender, e) =>
+        {
+            string[] dismissList = 
+            [
+                //nameof(IsShowString),
+            ];
+            //某个变量被更改
+            var name = e.PropertyName;
+            if (name == null || dismissList.Contains(name))
+                return;
+
+            var value = GetType().GetProperty(name)?.GetValue(this);
+            var valueString = value?.ToString() ?? "";
+            await Database.Set(name, valueString);
+        };
+    }
 }
